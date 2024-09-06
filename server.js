8a989e71d665044d19f68c9535da499b45329a85
Kaifkhan001 <kaifkhanksi7544@gmail.com>
2024-09-06 17:34:27 +0530

    handeling CORS policy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your frontend URL
+  origin: 'https://full-stack-attempt-amny-5pwurnfna-kaifkhan001s-projects.vercel.app', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }));
